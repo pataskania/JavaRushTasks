1d26d3bf2d951c53bf59488b7f3cb7f371f219f0
Dmitrii Faerman <143283648+pataskania@users.noreply.github.com>
2023-11-07 06:32:20 +0200

    hot and cold complete

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
@@ -6,8 +6,17 @@ import java.util.Scanner;
 Тепло или холодно
 */
 
+public class Solution {
+    public static void main(String[] args) {
+        String cold = "на улице холодно";
+        String warm = "на улице тепло";
+        Scanner scanner = new Scanner(System.in);
+        int tempt = scanner.nextInt();
+        if (tempt < 0){
+            System.out.println(cold);
+        }else 
+        System.out.println(warm);
+        
 
+    }
 }
-
-
-
