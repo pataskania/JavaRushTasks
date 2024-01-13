d1acc9fc6bd3f509ca93a4ee348bf8d97b5959b8
Dmitrii Faerman <143283648+pataskania@users.noreply.github.com>
2024-01-13 19:58:47 +0200

    add solution module JavaSyntax task 0403

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -10,30 +10,20 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
+
         while (true) {
             if (scanner.hasNextInt()) {
                 int number = scanner.nextInt();
                 sum += number;
             } else if (scanner.hasNextLine()) {
-                String string = scanner.nextLine();
-                if (string.equals("ENTER")) {
-                break;
+                String line = scanner.nextLine();
+                if (line.equals("ENTER")) {
+                    break;
                 }
             }
 
 
         }
         System.out.println(sum);
-
     }
-
-
 }
-
-
-
-
- 
-
-
-
