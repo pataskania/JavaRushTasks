f2d208c982e81c9854f18660c6c00a968601ff03
Dmitrii Faerman <143283648+pataskania@users.noreply.github.com>
2024-02-05 06:08:14 +0200

    add solution module JavaSyntax task 0702 && 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte b = 127;
+    short s = 32767;
+    int i = 2147483647;
+    long lo = 9223372036854775807l;
+    float f = Float.MAX_VALUE;
+            double d  = Double.MAX_VALUE;
+                    char c = 65_535;
+
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+byte b = Byte.MIN_VALUE;
+short s = Short.MIN_VALUE;
+int i = Integer.MIN_VALUE;
+long l = Long.MIN_VALUE;
+double d = Double.MIN_VALUE;
+char c = Character.MIN_VALUE;
+}
