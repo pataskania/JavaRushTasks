d077c5880cb9ef95a10e637e8508a8f0743a24f5
Dmitrii Faerman <143283648+pataskania@users.noreply.github.com>
2024-02-19 06:47:56 +0200

    add solution module JavaSyntax task 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String privet1 =  new String("Privet");
+        String privet2 =  new String("Privet");
+        String privet3 =  new String("Privet");
+        String privet4 =  new String("Privet");
+        String privet5 =  new String("Privet");
+        String privet6 =  new String("Privet");
+        String privet7 =  new String("Privet");
+        String privet8 =  new String("Privet");
+        String privet9 =  new String("Privet");
+        String privet10 =  new String("Privet");
+
+        int[] array1 = new int[5];
+        int[] array2 = new int[15];
+        int[] array3 = new int[25];
+        int[] array4 = new int[35];
+        int[] array5 = new int[45];
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+
+    }
+}
